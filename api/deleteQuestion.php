58d7c0b4cd3f9be39bcdd55c76e5c972fcb83395
pareshgami <gamiparesh5@gmail.com>
2020-10-26 10:10:29 +0530

    Delete question

--- api/deleteQuestion.php
+++ api/deleteQuestion.php
@@ -1,7 +1,10 @@
 <?php
+    header("Access-Control-Allow-Origin: *");
+    header('Access-Control-Allow-Headers: Content-Type, Authorization, X-Requested-With');
     require("Db.class.php");
     $db = new Db();
-    $qId = $_REQUEST['question_id'];
+    $json = json_decode(file_get_contents('php://input'), true);
+    $qId = $json['question_id'];
     $delete = $db->query("DELETE FROM question WHERE id = :id",array("id"=> $qId));
     $result = array("status" => 200, 'message' => 'Question deleted.!');
     echo json_encode($result);
